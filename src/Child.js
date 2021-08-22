7804cf4a8650043041c9436a7a1f73915d67d86c
hamzabdr36 <53819420+hamzabdr36@users.noreply.github.com>
2021-08-23 03:38:26 +0500

    initial

---
+++ src/Child.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Child(props) {
+  return (
+    <div >
+    CHILD
+    </div>
+  );
+}
+
+export default Child;
---
+++ src/Parent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Child from './Child';
+
+function Parent(props) {
+  return (
+    <div >
+        PARENT
+    <Child></Child>
+    </div>
+  );
+}
+
+export default Parent;
